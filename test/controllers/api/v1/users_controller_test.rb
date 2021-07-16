c692808655dc400ba9b95307fd1f5ff2903f456e
vino <vino0908@outlook.com>
2021-07-16 22:06:28 +0800

    add index action

--- test/controllers/api/v1/users_controller_test.rb
+++ test/controllers/api/v1/users_controller_test.rb
@@ -1,7 +1,8 @@
 require 'test_helper'
 
 class Api::V1::UsersControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  test 'index success should show all user' do
+    get api_v1_users_path as: :json
+    assert_response 200
+  end
 end
