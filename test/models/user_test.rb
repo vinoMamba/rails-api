bb6936201dea1c31b43c3692fb4784db77607b02
vino <vino0908@outlook.com>
2021-07-16 21:46:58 +0800

    add test

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,8 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test 'valid user with all valid ' do
+    user = User.new({ email: 'test@test.com', password_digest: '123456', role: 1 })
+    assert user.valid?
+  end
 end
